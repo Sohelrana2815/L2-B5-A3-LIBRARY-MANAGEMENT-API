6833697e55be3995b30d056baced926c0317cef1
Sohelrana2815 <sohel152302@gmail.com>
2025-06-21 15:12:36 +0600

    solved .env problem

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to library management app.");
+  res.send("Welcome to library management app📚.");
 });
 
 export default app;
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
 import { Server } from "http";
 import app from "./app";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 let server: Server;
 
@@ -7,7 +11,11 @@ const PORT = 5000;
 
 async function main() {
   try {
-    server = app.listen(PORT, () => {
+    server = app.listen(PORT, async () => {
+      await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pns8cwr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      );
+      console.log("Connect to MongoDB with Mongoose successfully✅");
       console.log(`Library management app is running🏃🏼‍♂️ on port ${PORT}`);
     });
   } catch (error) {
