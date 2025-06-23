38b692cf6d3039d575d39f929a64c3d2e8532885
Sohelrana2815 <sohel152302@gmail.com>
2025-06-23 07:04:48 +0600

    ff

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Book } from "../models/books.model";
 import { z } from "zod";
+import { ApiError } from "../../utils/ApiError";
 
 export const booksRoutes = express.Router();
 
@@ -23,68 +24,110 @@ const bookZodSchema = z.object({
 });
 
 // CREATE A BOOK
-booksRoutes.post("/books", async (req: Request, res: Response) => {
-  const bookPayload = await bookZodSchema.parseAsync(req.body);
-  console.log(bookPayload, "zod body");
+booksRoutes.post(
+  "/books",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Zod validation error if req.body is invalid data
+      const bookPayload = await bookZodSchema.parseAsync(req.body);
+      console.log(bookPayload, "post");
+      const created = await Book.create(bookPayload);
 
-  // const book = await Book.create(bookPayload);
-
-  res.status(201).json({
-    success: true,
-    message: "Book created successfully",
-    data: {},
-  });
-});
+      res.status(201).json({
+        success: true,
+        message: "Book created successfully",
+        data: created,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 // GET ALL BOOKS
 
-booksRoutes.get("/books", async (req: Request, res: Response) => {
-  const books = await Book.find({});
-  res.status(200).json({
-    success: true,
-    message: "Books retrieved successfully",
-    data: books,
-  });
-});
+booksRoutes.get(
+  "/books",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const books = await Book.find({});
+      res.status(200).json({
+        success: true,
+        message: "Books retrieved successfully",
+        data: books,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 // GET SINGLE BOOK (BY ID)
 
-booksRoutes.get("/books/:bookId", async (req: Request, res: Response) => {
-  const bookId = req.params.bookId;
-
-  const book = await Book.findById(bookId);
-
-  res.status(200).json({
-    success: true,
-    message: "Book retrieved successfully",
-    data: book,
-  });
-});
+booksRoutes.get(
+  "/books/:bookId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const book = await Book.findById(req.params.bookId);
+      if (!book) {
+        // throw 404 error from ApiError
+        throw new ApiError(404, "Book not found");
+      }
+      res.status(200).json({
+        success: true,
+        message: "Book retrieved successfully",
+        data: book,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 // UPDATE A SINGLE BOOK
-booksRoutes.put("/books/:bookId", async (req: Request, res: Response) => {
-  const bookId = req.params.bookId;
-  const bookToUpdate = req.body;
-  const updatedBook = await Book.findByIdAndUpdate(bookId, bookToUpdate, {
-    new: true,
-  });
+booksRoutes.put(
+  "/books/:bookId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
+    try {
+      // Zod validation
+      const bookPayload = await bookZodSchema.partial().parseAsync(req.body);
 
-  res.status(200).json({
-    success: true,
-    message: "Book updated successfully",
-    data: updatedBook,
-  });
-});
+      const updated = await Book.findByIdAndUpdate(bookId, bookPayload, {
+        new: true,
+        runValidators: true,
+      });
+
+      if (!updated) throw new ApiError(404, "Book not found");
+      res.status(200).json({
+        success: true,
+        message: "Book updated successfully",
+        data: updated,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 // DELETE A BOOK
 
-booksRoutes.delete("/books/:bookId", async (req: Request, res: Response) => {
-  const bookId = req.params.bookId;
+booksRoutes.delete(
+  "/books/:bookId",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
 
-  await Book.findByIdAndDelete(bookId);
-  res.status(200).json({
-    success: true,
-    message: "Book deleted successfully",
-    data: null,
-  });
-});
+    try {
+      const deleted = await Book.findByIdAndDelete(bookId);
+
+      if (!deleted) throw new ApiError(404, "Book not found");
+      res.status(200).json({
+        success: true,
+        message: "Book deleted successfully",
+        data: null,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
