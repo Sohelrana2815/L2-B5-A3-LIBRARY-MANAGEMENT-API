56412272962f6f39108df3aeb05cf4b630cfaea1
Sohelrana2815 <sohel152302@gmail.com>
2025-09-16 12:22:10 +0600

    ddd

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,15 @@ import cors from "cors";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"] }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://library-management-api-flame.vercel.app/api",
+    ],
+  })
+);
 
 app.use("/api", booksRoutes);
 app.use("/api", borrowRoutes);
